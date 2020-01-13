8a01e348fbbc5180fe4e06ab947a43579e1f5600
Stefan Grotz <stefan.grotz@1und1.de>
2020-01-13 22:48:59 +0100

    languagetool-check.sh added

---
+++ languagetool-check.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# jq https://stedolan.github.io/jq/
+
+# insert username and API key from https://languagetoolplus.com/api-access
+username='example@domain.me'
+apiKey='3xamPl3'
+
+while IFS= read -r line; do
+  
+  matchesJSON=$(
+  curl -s -X POST --header 'Content-Type: application/x-www-form-urlencoded' --header 'Accept: application/json' -d 'text='"$line"'&language=eo&username='"$username"'&apiKey='"$apiKey"'&enabledOnly=false' 'https://api.languagetoolplus.com/v2/check' |\
+  jq '.matches')
+  
+  matchesJSONLength=$(echo -n $matchesJSON | wc -c)
+  #  echo $matchesJSONLength
+  
+  if [ "$matchesJSONLength" -eq "6" ]; then
+        echo "$line"
+  fi
+  
+  sleep 2
+done < "$1"
